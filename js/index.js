481f1fba3222e69a7ca26e1c5ecb4f2e8216e71d
dokkillo <devgcosta@gmail.com>
2015-08-30 17:45:21 +0200

    new version

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
- $("#menu-toggle").click(function(e) {
-   e.preventDefault();
-   $("#wrapper").toggleClass("toggled");
- });
-var $toTop = $('.toTop');
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 2) {
-        $toTop.fadeIn("slow");
-    } else {             
+ $(document).ready(function() {
+
+   $("#menu-toggle").click(function(e) {
+     e.preventDefault();
+     $("#wrapper").toggleClass("toggled");
+   });
+   var $toTop = $('.toTop');
+   $(window).scroll(function() {
+     if ($(this).scrollTop() > 200) {
+       $toTop.fadeIn("slow");
+     } else {
        $toTop.fadeOut("slow");
-    }
-});
+     }
+   });
+
+   $(".ukFlagAboutMe").click(function() {
+     $(".spanish").fadeOut(function() { 
+       $(".english").fadeIn("slow");
+     });
+   });
+   $(".spainFlagAboutMe").click(function() {  
+     $(".english").fadeOut(function  () { 
+       $(".spanish").fadeIn("slow");
+     });
+   });
+
+ });
